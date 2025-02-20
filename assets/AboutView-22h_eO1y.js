463e26db931f4e1043592bb180cca7e7d3862916
Bassel Aflak <b7a@hotmail.de>
2025-02-20 21:23:00 +0300

    added dist

---
+++ assets/AboutView-22h_eO1y.js
@@ -0,0 +1,1 @@
+import{g as s,f as t,h as o,b as a}from"./index-SSA2PKfU.js";const n={},r={class:"about"};function c(i,e){return a(),t("div",r,e[0]||(e[0]=[o("h1",null,"This is an about page",-1)]))}const f=s(n,[["render",c]]);export{f as default};
